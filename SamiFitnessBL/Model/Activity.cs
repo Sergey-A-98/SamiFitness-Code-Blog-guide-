197f57a9378fa49a98292212f5e7f0435bcbab47
Сами <serjmercurial@gmail.com>
2024-06-20 02:23:27 +0500

    Добавлена возможность ввода активности

---
+++ SamiFitnessBL/Model/Activity.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SamiFitnessBL.Model
+{
+    [Serializable]
+    public class Activity
+    {
+        public string Name { get; }
+        public double CaloriesPerMinute { get; }
+
+        public Activity(string name, double caloriesPerMinute)
+        {
+            //TODO: проверка
+            Name = name;
+            CaloriesPerMinute = caloriesPerMinute;
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
--- SamiFitnessCMD/Program.cs
+++ SamiFitnessCMD/Program.cs
@@ -27,11 +27,12 @@ namespace SamiFitnessCMD
 
             var userController = new UserController(name);
             var eatingController = new EatingController(userController.CurrentUser);
+            var exerciseController = new ExerciseController(userController.CurrentUser);
             if (userController.IsNewUser)
             {
                 Console.Write("Введите пол: ");
                 var gender = Console.ReadLine();
-                var birthDate = ParseDateTime();
+                var birthDate = ParseDateTime("дата рождения");
                 var weight = ParseDouble("вес");
                 var height = ParseDouble("рост");
 
@@ -41,24 +42,55 @@ namespace SamiFitnessCMD
             }
             Console.WriteLine(userController.CurrentUser);
 
-            Console.WriteLine("Что вы хотите сделать?");
-            Console.WriteLine("Е - ввести прием пищи");
-            var key = Console.ReadKey();
-            Console.WriteLine();
-            Console.ReadLine();
 
-            if(key.Key == ConsoleKey.E)
+            while (true)
             {
-                var foods =  EnterEating();
-                eatingController.Add(foods.Food, foods.Weight);
-
-                foreach(var item in eatingController.Eating.Foods)
+                Console.WriteLine("Что вы хотите сделать?");
+                Console.WriteLine("E - ввести прием пищи");
+                Console.WriteLine("A - ввести упражнение");
+                Console.WriteLine("Q - выход");
+                var key = Console.ReadKey();
+                Console.WriteLine();
+                switch (key.Key)
                 {
-                    Console.WriteLine($"\t{item.Key} - {item.Value}");
+                    case ConsoleKey.E:
+                        var foods = EnterEating();
+                        eatingController.Add(foods.Food, foods.Weight);
+
+                        foreach (var item in eatingController.Eating.Foods)
+                        {
+                            Console.WriteLine($"\t{item.Key} - {item.Value}");
+                        }
+                        break;
+                    case ConsoleKey.A:
+                        var exe = EnterExercise();
+                        exerciseController.Add(exe.Activity, exe.Begin, exe.End);
+                        foreach(var item in exerciseController.Exercises)
+                        {
+                            Console.WriteLine($"\t{item.Activity} с {item.Start.ToShortTimeString()} до {item.Finish.ToShortTimeString()}");
+                        }
+                        break;
+                    case ConsoleKey.Q:
+                        Environment.Exit(0);
+                        break;
                 }
             }
 
             Console.ReadLine();
+        }
+
+        private static (DateTime Begin, DateTime End, Activity Activity) EnterExercise()
+        {
+            Console.Write("Введите название упражнения: ");
+            var name = Console.ReadLine();
+
+            var energy = ParseDouble("расход энергии в минуту");
+
+            var begin = ParseDateTime("начало упражнения");
+            var end = ParseDateTime("окончание упражнения");
+            var activity = new Activity(name, energy);
+
+            return (begin, end, activity);
         }
 
         private static (Food Food, double Weight)  EnterEating()
@@ -78,19 +110,19 @@ namespace SamiFitnessCMD
             return (Food: product, Weight: weight);
         }
 
-        private static DateTime ParseDateTime()
+        private static DateTime ParseDateTime(string value)
         {
             DateTime birthDate;
             while (true)
             {
-                Console.Write("Введите дату рождения (dd.MM.yyyy): ");
+                Console.Write($"Введите {value} (dd.MM.yyyy): ");
                 if (DateTime.TryParse(Console.ReadLine(), out birthDate))
                 {
                     break;
                 }
                 else
                 {
-                    Console.WriteLine("Неверный формат даты рождения");
+                    Console.WriteLine($"Неверный формат {value}");
                 }
             }
 
